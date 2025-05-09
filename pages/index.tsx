216e2ca3868e12833fea814e0b215ec8190e6b95
ragnaar12 <houssembenahmed11@gmail.com>
2025-05-09 23:37:14 +0100

    index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the RAG Web Application</h1>
+            <p>This is the landing page of the application.</p>
+        </div>
+    );
+};
+
+export default HomePage;
