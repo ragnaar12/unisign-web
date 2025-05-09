4e83fa1f53a4180f0fd495a4683412877144d702
ragnaar12 <houssembenahmed11@gmail.com>
2025-05-09 23:40:45 +0100

    main.tsx

---
+++ src/utlis/main.tsx
@@ -0,0 +1,9 @@
+import App from './components/App';
+import './styles/global.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
